77e777454f9a229e7005fd4a8563eed9bac57c61
mary010-sky <mc2023076791@virtual.upt.pe>
2026-05-02 02:44:09 -0500

    Delete backend directory

--- backend/core/__init__.py
+++
@@ -1,1 +0,0 @@
-# VulnScan Backend Package
--- backend/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""
-Pytest configuration for VulnScan backend tests.
-"""
-import sys
-import os
-
-# Make sure the backend root is always on the path
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
